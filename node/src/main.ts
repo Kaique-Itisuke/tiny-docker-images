e03bc4e4e246c0ea0cb4984d3905ca0ef1d3a0fd
Kaique Itisuke <kaiquel2@hotmail.com>
2025-03-05 00:46:16 -0300

    fix: bind Fastify server to all network interfaces

--- node/src/main.ts
+++ node/src/main.ts
@@ -8,7 +8,7 @@ fastify.get('/health', (request: FastifyRequest, reply: FastifyReply) => {
   reply.send({ message: 'healthy' })
 })
 
-fastify.listen({ port: 8080 }, (err: Error | null, address: string) => {
+fastify.listen({ port: 8080, host: '0.0.0.0' }, (err: Error | null, address: string) => {
   if (err) {
     fastify.log.error(err)
     process.exit(1)
